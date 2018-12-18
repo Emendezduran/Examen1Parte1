94cfde234e81cf38d39cd3bd94a5258a491c9b1a
emendezduran <emendezduran@nautilus25.danielcastelao.org>
2018-12-18 09:05:38 +0100

    format Factorial

---
+++ factorial/Factorial.java
@@ -0,0 +1,26 @@
+package factorial;
+
+public class Factorial {
+
+    public static void main(String[] args) {
+
+        int numeroJ;
+        int numeroF;
+
+        numeroJ = 8;
+
+        int contador;
+        if (numeroJ == 0) {
+            numeroF = 1;
+        } else {
+            numeroF = 1;
+            for (contador = numeroJ; contador >= 1; contador--) {
+                numeroF = numeroF * contador;
+            }
+        }
+
+        System.out.println(numeroF);
+
+    }
+
+}
