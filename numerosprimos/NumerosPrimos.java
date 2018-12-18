2691ea68da033c9e29798823e821fbc9937875e9
emendezduran <emendezduran@nautilus25.danielcastelao.org>
2018-12-18 09:18:22 +0100

    format numerosPrimos

--- numerosprimos/NumerosPrimos.java
+++ numerosprimos/NumerosPrimos.java
@@ -2,7 +2,7 @@ package numerosprimos;
 
 /**
  *
- * @author 
+ * @author
  */
 public class NumerosPrimos {
 
@@ -13,59 +13,61 @@ public class NumerosPrimos {
     //LIstar los numeros según el numero de digitos indicado
     //Considero solo hasta numeros menores a 100000 (5 digitos), 
     //por el hecho de k buscar numeros primos a partir de 6 digitos, el proceso se hace muy lento.
-
     public static boolean p = false;
-    
-    public static void main(String arg[])
-        {
-        int numeroDigitos=0;
-        int digitos=0;
-        numeroDigitos=Integer.parseInt(arg[0]);
-        if(numeroDigitos<=0)
-        System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
-        for(int i = 1; i <= 99999; i++ )
-        {
-            int divisionEntera = i;
- 
-            int contador=0;
- 
-            while (divisionEntera != 0)
-        {
-            divisionEntera = divisionEntera / 10;
-            contador++;
+
+    public static void main(String arg[]) {
+        int numeroDigitos = 0;
+        int digitos = 0;
+        numeroDigitos = Integer.parseInt(arg[0]);
+        if (numeroDigitos <= 0) {
+            System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
         }
-        digitos=contador;
-            
-            
-            if(digitos==numeroDigitos){
-                   if (i < 4) p = true;
-            else
-            {
-            if (i % 2 == 0) p = false; 
-                else
-                {
-                    int contador1 = 0;  
-                    int numero1 = 1; 
-                    int limite = (i - 1) / 2; 
-                    if  (limite % 2 == 0) limite--;  
-             
-                    while(numero1 <= limite)
-                    {
-                        if (i % numero1 == 0) contador1++;
+        for (int i = 1; i <= 99999; i++) {
+            int divisionEntera = i;
+
+            int contador = 0;
+
+            while (divisionEntera != 0) {
+                divisionEntera = divisionEntera / 10;
+                contador++;
+            }
+            digitos = contador;
+
+            if (digitos == numeroDigitos) {
+                if (i < 4) {
+                    p = true;
+                } else {
+                    if (i % 2 == 0) {
+                        p = false;
+                    } else {
+                        int contador1 = 0;
+                        int numero1 = 1;
+                        int limite = (i - 1) / 2;
+                        if (limite % 2 == 0) {
+                            limite--;
+                        }
+
+                        while (numero1 <= limite) {
+                            if (i % numero1 == 0) {
+                                contador1++;
+                            }
                             numero1 += 2;
-                            if (contador1 == 2) numero1 = limite + 1;
+                            if (contador1 == 2) {
+                                numero1 = limite + 1;
+                            }
+                        }
+
+                        if (contador1 == 1) {
+                            p = true;
+                        }
                     }
- 
-        if (contador1 == 1) p = true;
-            } 
-        } 
-     
-                if (p == true) 
-                    System.out.println(i);    
+                }
+
+                if (p == true) {
+                    System.out.println(i);
+                }
             }
         }
-        }
- 
-       
- 
+    }
+
 }
